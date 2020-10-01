b023d228cfb1b10a046cbe75f015d99e507572cc
joli <1040123159@qq.com>
2020-10-01 23:10:30 +0800

    添加404页面

---
+++ src/views/NotFound.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div>当前页面不存在，请检查链接是否正确</div>
+    <div>
+      <router-link to="/">返回首页</router-link>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+@Component
+export default class NotFound extends Vue{
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
