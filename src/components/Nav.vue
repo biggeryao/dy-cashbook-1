277886889737158fff199b664da68370879a3c18
joli <1040123159@qq.com>
2020-10-01 23:51:51 +0800

    使用icon，封装Icon组件

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,29 @@
 <template>
-<div>
-  <router-link to="/money">记账</router-link>
-  <router-link to="/labels">标签</router-link>
-  <router-link to="/statistics">统计</router-link>
-</div>
+  <nav>
+    <router-link to="/money" class="item" active-class="selected">
+      <Icon name="money"/>
+      记账
+    </router-link>
+    <router-link to="/labels" class="item" active-class="selected">
+      <Icon name="label"/>
+      标签
+    </router-link>
+    <router-link to="/statistics" class="item" active-class="selected">
+      <Icon name="statistics"/>
+      统计
+    </router-link>
+  </nav>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
+
+
+
+
+
+
 @Component
 export default class Nav extends Vue{
 
