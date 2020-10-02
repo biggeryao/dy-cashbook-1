f48b461eb317122962f10f35a9e3d978954c3b61
joli <1040123159@qq.com>
2020-10-02 16:44:26 +0800

    实现types切换

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -1,16 +1,32 @@
 <template>
   <div>
     <ul class="types">
-      <li class="selected">支出</li>
-      <li>收入</li>
+      <li :class="type==='-' && 'selected'"
+          @click="selectType('-')">支出
+      </li>
+      <li :class="type==='+' && 'selected'"
+          @click="selectType('+')">收入
+      </li>
     </ul>
   </div>
 </template>
 
 <script lang="ts">
-export default {
-  name: 'Types'
-};
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+
+@Component
+export default class Types extends Vue {
+
+  type = '-';
+
+  selectType(type: string) {
+    if (type !== '-' && type !== '+') {
+      throw new Error('type is unKnow');
+    }
+    this.type = type;
+  }
+}
 </script>
 
 <style lang="scss" scoped>
@@ -19,6 +35,7 @@ export default {
   display: flex;
   text-align: center;
   font-size: 24px;
+
   > li {
     width: 50%;
     height: 64px;
@@ -26,6 +43,7 @@ export default {
     justify-content: center;
     align-items: center;
     position: relative;
+
     &.selected::after {
       content: '';
       position: absolute;
