0513492c4cfbec9c9beda586a4f2ca20f274bf34
joli <1040123159@qq.com>
2020-10-04 17:46:51 +0800

    完成tags组件

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -1,38 +1,47 @@
 <template>
   <div class="tags">
     <div class="new">
-      <button>新增标签</button>
+      <button @click="create">新增标签</button>
     </div>
     <ul class="current">
-      <li>衣</li>
-      <li>食</li>
-      <li>住</li>
-      <li>行</li>
-      <li>衣</li>
-      <li>食</li>
-      <li>住</li>
-      <li>行</li>
-      <li>衣</li>
-      <li>食</li>
-      <li>住</li>
-      <li>行</li>
-      <li>衣</li>
-      <li>食</li>
-      <li>住</li>
-      <li>行</li>
-      <li>衣</li>
-      <li>食</li>
-      <li>住</li>
-      <li>行</li>
+      <li v-for="tag in dataSource" :key="tag"
+          :class="{selected:selectedTags.indexOf(tag)>=0}"
+          @click="toggle(tag)"
+      >{{ tag }}
+      </li>
     </ul>
   </div>
 
 </template>
 
 <script lang="ts">
-export default {
-  name: 'Tags'
-};
+import Vue from 'vue';
+import {Component, Prop} from 'vue-property-decorator';
+
+@Component
+export default class Tags extends Vue {
+  @Prop(Array) dataSource: string[] | undefined;
+  selectedTags: string[] = [];
+
+  toggle(tag: string) {
+    console.log(tag);
+    const index=this.selectedTags.indexOf(tag)
+    if(index>=0){
+      this.selectedTags.splice(index,1)
+    }else {
+      this.selectedTags.push(tag);
+    }
+  }
+
+  create(){
+    const name =prompt('请输入标签名')
+    if(name===''){
+      window.alert('标签名不能为空')
+    }else if (this.dataSource){
+        this.$emit('update:dataSource',[...this.dataSource,name])
+      }
+    }
+}
 </script>
 
 <style lang="scss" scoped>
@@ -42,11 +51,14 @@ export default {
   flex-grow: 1;
   display: flex;
   flex-direction: column-reverse;
+
   > .current {
     display: flex;
     flex-wrap: wrap;
+
     > li {
-      background: #d9d9d9;
+      $bg: #d9d9d9;
+      background: $bg;
       $h: 24px;
       height: $h;
       line-height: $h;
@@ -54,10 +66,15 @@ export default {
       padding: 0 16px;
       margin-right: 12px;
       margin-top: 4px;
+      &.selected{
+        background: darken($bg,50%);
+      }
     }
   }
+
   > .new {
     padding-top: 16px;
+
     button {
       background: transparent;
       border: none;
