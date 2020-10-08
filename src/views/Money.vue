3cdb9b51fc2eb4df4b1cd153b0615b66e2614a94
joli <1040123159@qq.com>
2020-10-08 19:07:22 +0800

    将所有组件封装到 store里

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -3,11 +3,11 @@
     <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
     <Types :value.sync="record.type"/>
     <FormItem @update:value="onUpdateNotes"
-           fired-name="备注"
-           placeholder="在这里输入备注"
+              fired-name="备注"
+              placeholder="在这里输入备注"
     />
-    <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
-    {{record}}
+    <Tags/>
+    {{ record }}
   </Layout>
 </template>
 
@@ -18,9 +18,7 @@ import Types from '@/components/Money/Types.vue';
 import FormItem from '@/components/Money/FormItem.vue';
 import Tags from '@/components/Money/Tags.vue';
 import {Component} from 'vue-property-decorator';
-
-
-
+import store from '@/store/index2';
 
 
 @Component({
@@ -28,20 +26,16 @@ import {Component} from 'vue-property-decorator';
 })
 export default class Money extends Vue {
 
-  tags = window.tagList;
-  recordList=window.recordList
-  record: RecordItem = {tags: [], notes: '', type: '-', amount: 0};
 
-  onUpdateTags(value: string[]) {
-    this.record.tags = value;
-  }
+  recordList = store.recordList;
+  record: RecordItem = {tags: [], notes: '', type: '-', amount: 0};
 
   onUpdateNotes(value: string) {
     this.record.notes = value;
   }
 
   saveRecord() {
-    window.createRecord
+    store.createRecord(this.record);
   }
 
 
@@ -53,7 +47,8 @@ export default class Money extends Vue {
   display: flex;
   flex-direction: column-reverse;
 }
-.notes{
+
+.notes {
   padding: 12px 0;
 }
 </style>
