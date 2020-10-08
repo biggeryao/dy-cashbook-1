476c95fb130cb1844d8485409195aba7f0765a19
joli <1040123159@qq.com>
2020-10-08 16:37:06 +0800

    封装record

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -17,11 +17,10 @@ import NumberPad from '@/components/Money/NumberPad.vue';
 import Types from '@/components/Money/Types.vue';
 import FormItem from '@/components/Money/FormItem.vue';
 import Tags from '@/components/Money/Tags.vue';
-import {Component, Watch} from 'vue-property-decorator';
-import {recordListModel} from '@/models/recordListModel';
-import {tagListModel} from '@/models/tagListModel';
+import {Component} from 'vue-property-decorator';
 
-const recordList = recordListModel.fetch();
+
+
 
 
 @Component({
@@ -30,7 +29,7 @@ const recordList = recordListModel.fetch();
 export default class Money extends Vue {
 
   tags = window.tagList;
-  recordList: RecordItem[] = recordList;
+  recordList=window.recordList
   record: RecordItem = {tags: [], notes: '', type: '-', amount: 0};
 
   onUpdateTags(value: string[]) {
@@ -42,13 +41,10 @@ export default class Money extends Vue {
   }
 
   saveRecord() {
-    recordListModel.create(this.record)
+    window.createRecord
   }
 
-  @Watch('recordList')
-  onRecordListChanged() {
-    recordListModel.save();
-  }
+
 }
 </script>
 
