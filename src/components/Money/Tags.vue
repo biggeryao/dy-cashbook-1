8d0dbc45fdb24316ba4aa07c50998ff48334e969
joli <1040123159@qq.com>
2020-10-08 19:08:00 +0800

    将所有组件封装到 store里

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -4,7 +4,7 @@
       <button @click="create">新增标签</button>
     </div>
     <ul class="current">
-      <li v-for="tag in dataSource" :key="tag.id"
+      <li v-for="tag in tagList" :key="tag.id"
           :class="{selected:selectedTags.indexOf(tag)>=0}"
           @click="toggle(tag)"
       >{{ tag.name }}
@@ -16,11 +16,12 @@
 
 <script lang="ts">
 import Vue from 'vue';
-import {Component, Prop} from 'vue-property-decorator';
+import {Component} from 'vue-property-decorator';
+import store from '@/store/index2';
 
 @Component
 export default class Tags extends Vue {
-  @Prop(Array) dataSource: string[] | undefined;
+  tagList=store.fetchTags()
   selectedTags: string[] = [];
 
   toggle(tag: string) {
@@ -35,10 +36,10 @@ export default class Tags extends Vue {
 
   create(){
     const name =prompt('请输入标签名')
-    if(name===''){
-      window.alert('标签名不能为空')
-    }else if (this.dataSource){
-        this.$emit('update:dataSource',[...this.dataSource,name])
+    if(!name){
+      return window.alert('标签名不能为空')
+    }else {
+       store.createTag(name)
       }
     }
 }
---
+++ src/store/index2.ts
@@ -0,0 +1,8 @@
+import recordStore from '@/store/recordStore';
+import tagStore from '@/store/tagStore';
+
+const store = {
+  ...recordStore,
+  ...tagStore,
+};
+export default store;
